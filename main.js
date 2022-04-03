30db00ec604ed3075f0e60675a3dfed6a6a830bd
Teibok <teibokchyne@gmail.com>
2022-04-03 21:10:31 +0530

    3rd April

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
     
     let btnAddFolder = document.querySelector("#addFolder");
     let btnAddTextFile = document.querySelector("#addTextFile");
+    let btnAddAlbum = document.querySelector("#addAlbum");
     let divbreadcrumb = document.querySelector("#breadcrumb");
     let divContainer = document.querySelector("#container");
     let templates = document.querySelector("#templates");
@@ -14,6 +15,7 @@
     let divAppTitle = document.querySelector("#app-title");
     let divAppMenuBar = document.querySelector("#app-menu-bar");
     let divAppBody = document.querySelector("#app-body");
+    let appClose = document.querySelector("#app-close");
 
     let resources = [];
     let rid = -1;
@@ -22,6 +24,57 @@
     aRootPath.addEventListener("click", viewFolderFromPath);
     btnAddFolder.addEventListener("click",addFolder);
     btnAddTextFile.addEventListener("click", addTextFile);
+    btnAddAlbum.addEventListener("click", addAlbum);
+    appClose.addEventListener("click", closeApp);
+
+    function addAlbum(){
+        let rname = prompt("Enter album's name");
+        if(rname != null){
+            rname = rname.trim();
+        }
+
+        if(!rname){
+            alert("Empty name not valid");
+            return;
+        }
+
+        let alreadyExists = resources.some(r => r.rname == rname && r.pid == cfid);
+        if(alreadyExists == true){
+            alert(rname + "is already exists");
+            return;
+        }
+
+        let pid = cfid;
+        rid++;
+        addAlbumHTML(rname, rid, pid);
+        resources.push({
+            rid : rid,
+            rname: rname,
+            rtype: "album",
+            pid: cfid
+        });
+        saveToStorage();
+    }
+
+    function addAlbumHTML(rname, rid, pid){
+        let divAlbumTemplate = templates.content.querySelector(".album");
+        let divAlbum = document.importNode(divAlbumTemplate,true);
+
+        let spanRename = divAlbum.querySelector("[action=rename");
+        let spanDelete = divAlbum.querySelector("[action=delete");
+        let spanView = divAlbum.querySelector("[action=view");
+        let divName = divAlbum.querySelector("[purpose=name]");
+
+        spanRename.addEventListener("click", renameAlbum);
+        spanDelete.addEventListener("click", deleteAlbum);
+        spanView.addEventListener("click", viewAlbum);
+        divName.innerHTML = rname;
+
+        divAlbum.setAttribute("rid", rid);
+        divAlbum.setAttribute("pid", pid);
+
+        divContainer.appendChild(divAlbum);
+    }
 
     function addFolder(){
         let rname = prompt("Enter folder's name");
@@ -66,6 +119,19 @@
         divFolder.setAttribute("pid", pid);
 
         divContainer.appendChild(divFolder);
+    }
+
+    function addPictureToAlbum(){
+        let iurl = prompt("Eneter an image url");
+        if(!iurl){
+            return;
+        }
+        let img = document.createElement("img");
+        img.setAttribute("src", iurl);
+        img.addEventListener("click", showPictureInMain);
+
+        let divPictureList = divAppBody.querySelector(".picture-list");
+        divPictureList.appendChild(img);
     }
 
     function addTextFile(){
@@ -143,7 +209,35 @@
         let color = this.value;
         let textArea = divAppBody.querySelector("textArea");
         textArea.style.color = color;
-    }    
+    }   
+    
+    function closeApp(){
+        divAppTitle.innerHTML = "title will come here";
+        divAppTitle.setAttribute("rid", "");
+        divAppMenuBar.innerHTML = "";
+        divAppBody.innerHTML = ""; 
+    }
+
+    function deleteAlbum(){
+        let spanDelete = this;
+        let divAlbum = spanDelete.parentNode;
+        let divName = divAlbum.querySelector("[purpose='name']");
+
+        let fidTBD = parseInt(divAlbum.getAttribute("rid"));
+        let fname = divName.innerHTML;
+
+        let sure = confirm(`Are you sure you want to deler ${fname}?`);
+        if(!sure){
+            return;
+        }
+        //html
+        divContainer.removeChild(divAlbum);
+        //ram
+        let ridx = resources.findIndex(r => r.rid == fidTBD);
+        resources.splice(ridx, 1);
+        //storage
+        saveToStorage();
+    }
 
     function deleteFolder(){
         //delete all folders inside also aka recursively
@@ -200,7 +294,19 @@
     }
 
     function downloadNotepad(){
-        console.log("in download");
+        let rid = parseInt(divAppTitle.getAttribute("rid"));
+        let resource = resources.find( r => r.rid == rid);
+        let divNotepadMenu = this.parentNode;
+
+        let strForDownload = JSON.stringify(resource);
+        let encodedData = encodeURIComponent(strForDownload);
+
+        let aDownload = divNotepadMenu.querySelector("a[purpose=download]");
+        aDownload.setAttribute("href", "data:text/json; charset=utf-8, "+ encodedData);
+        aDownload.setAttribute("download", resource.rname+".json");
+
+        aDownload.click();
+
     }
 
     function makeNotepadBold(){
@@ -237,6 +343,42 @@
             this.setAttribute("pressed", false);
             textArea.style.textDecoration = "none";
         }
+    }
+
+    function renameAlbum(){
+        let nrname = prompt("Enter album's name");
+        if(nrname != null){
+            nrname = nrname.trim();
+        }  
+
+        if(!nrname){//empty name validation
+            alert("Empty name is not allowed");
+            return;
+        }
+
+        let spanRename = this;
+        let divAlbum = spanRename.parentNode;
+        let divName = divAlbum.querySelector("[purpose=name");
+        let orname = divName.innerHTML;
+        let ridTBU = parseInt(divAlbum.getAttribute("rid"));
+        if(nrname == orname){
+            alert("Please enter a new name.");
+            return;
+
+        }
+
+        let alreadyExists = resources.some(r => r.name == nrname && r.pid == cfid);
+        if(alreadyExists == true){
+            alert(nrname + " already exists.");
+            return;
+        }
+        //change html
+        divName.innerHTML = nrname;
+        //change ram
+        let resource = resources.find(r => r.rid == ridTBU);
+        resource.rname = nrname;
+        //change storage
+        saveToStorage(); 
     }
 
     function renameFolder(){
@@ -341,8 +483,77 @@
         localStorage.setItem("data", rjson);
     }
 
+    function showPictureInMain(){
+        let divPictureMainImg = divAppBody.querySelector(".picture-main > img");
+        divPictureMainImg.setAttribute("src", this.getAttribute("src"));
+        
+        let imgs = divAppBody.querySelectorAll(".picture-list > img");
+        for(let i=0; i<imgs.length ; ++i){
+            imgs[i].setAttribute("pressed", false);
+        }
+        this.setAttribute("pressed", true);
+    }
+
     function uploadNotepad(){
-        console.log("in upload");
+        let file = window.event.target.files[0];
+        let reader = new FileReader();
+        reader.addEventListener("load", function(){
+            let data = window.event.target.result;
+            let resource = JSON.parse(data);
+
+            let spanSave = divAppMenuBar.querySelector("[action=save]");
+        let spanBold = divAppMenuBar.querySelector("[action=bold]");
+        let spanItalic = divAppMenuBar.querySelector("[action=italic]");
+        let spanUnderline = divAppMenuBar.querySelector("[action=underline]");
+        let inputBGColor = divAppMenuBar.querySelector("[action=bg-color]");
+        let inputTextColor = divAppMenuBar.querySelector("[action=fg-color]");
+        let selectFontFamily = divAppMenuBar.querySelector("[action=font-family]");
+        let selectFontSize = divAppMenuBar.querySelector("[action=font-size]");
+        let textArea = divAppBody.querySelector("textArea");
+
+        spanBold.setAttribute("pressed", !resource.isBold);
+        spanItalic.setAttribute("pressed", !resource.isItalic);
+        spanUnderline.setAttribute("pressed", !resource.isUnderline);
+        inputBGColor.value = resource.bgColor;
+        inputTextColor.value = resource.textColor;
+        selectFontFamily.value = resource.fontFamily;
+        selectFontSize.value = resource.fontSize;
+        textArea.value = resource.content;
+
+        spanBold.dispatchEvent(new Event("click"));
+        spanItalic.dispatchEvent(new Event("click"));
+        spanUnderline.dispatchEvent(new Event("click"));
+        inputBGColor.dispatchEvent(new Event("change"));
+        inputTextColor.dispatchEvent(new Event("change"));
+        selectFontFamily.dispatchEvent(new Event("change"));
+        selectFontSize.dispatchEvent(new Event("change"));
+
+        });
+        reader.readAsText(file);
+    }
+
+    function viewAlbum(){
+        let spanView = this;
+        let divAlbum = spanView.parentNode;
+        let divName = divAlbum.querySelector("[purpose=name]")
+        let fname = divName.innerHTML;
+        let fid = parseInt(divAlbum.getAttribute("rid"));
+
+        let divAlbumMenuTemplate  = templates.content.querySelector("[purpose=album-menu]");
+        let divAlbumMenu = document.importNode(divAlbumMenuTemplate, true);
+        divAppMenuBar.innerHTML = "";
+        divAppMenuBar.appendChild(divAlbumMenu);
+
+        let divAlbumBodyTemplate = templates.content.querySelector("[purpose=album-body]");
+        let divAlbumBody = document.importNode(divAlbumBodyTemplate, true);
+        divAppBody.innerHTML = "";
+        divAppBody.appendChild(divAlbumBody);
+
+        divAppTitle.innerHTML = fname;
+        divAppTitle.setAttribute("rid", fid);
+
+        let spanAdd = divAlbumMenu.querySelector("[action=add]");
+        spanAdd.addEventListener("click", addPictureToAlbum);
     }
 
     function viewFolder(){
@@ -375,8 +586,11 @@
         let fid = parseInt(aPath.getAttribute("rid"));
 
         //set teh breadcrumb
-        while(aPath.nextSibling){
-            aPath.parentNode.removeChild(aPath.nextSibling);
+        for(let i = divbreadcrumb.children.length -1; i>=0; i--){
+            if(divbreadcrumb.children[i] == aPath){
+                break;
+            }
+            divbreadcrumb.removeChild(divbreadcrumb.children[i]);
         }
 
         //set the container
@@ -384,7 +598,14 @@
         divContainer.innerHTML = "";
         for(let i=0; i<resources.length; ++i){
             if(resources[i].pid == cfid){
-                addFolderHTML(resources[i].rname, resources[i].rid, resources[i].pid)
+                if(resources[i].rtype == "folder"){
+                    addFolderHTML(resources[i].rname, resources[i].rid, resources[i].pid);
+                }else if(resources[i].rtype == "text-file"){
+                    addTextFileHTML(resources[i].rname, resources[i].rid, resources[i].pid);
+                }else if(resources[i].rtype == "album"){
+                    addAlbumHTML(resources[i].rname, resources[i].rid, resources[i].pid);
+                }
+                
             }
         }
     }
@@ -418,6 +639,7 @@
         let selectFontFamily = divAppMenuBar.querySelector("[action=font-family]");
         let selectFontSize = divAppMenuBar.querySelector("[action=font-size]");
         let spanDownload = divAppMenuBar.querySelector("[action=download]");
+        let spanForUpload = divAppMenuBar.querySelector("[action=forupload]");
         let inputUpload = divAppMenuBar.querySelector("[action=upload]");
         let textArea = divAppBody.querySelector("textArea");
 
@@ -431,6 +653,9 @@
         selectFontSize.addEventListener("change", changeNotepadFontSize);
         spanDownload.addEventListener("click", downloadNotepad);
         inputUpload.addEventListener("change", uploadNotepad);
+        spanForUpload.addEventListener("click", function(){
+            inputUpload.click();
+        })
 
         let resource = resources.find(r => r.rid == fid);
         spanBold.setAttribute("pressed", !resource.isBold);
@@ -462,6 +687,8 @@
                         addFolderHTML(resources[i].rname, resources[i].rid, resources[i].pid);
                     }else if(resources[i].rtype == "text-file"){
                         addTextFileHTML(resources[i].rname, resources[i].rid, resources[i].pid);
+                    }else if(resources[i].rtype == "album"){
+                        addAlbumHTML(resources[i].rname, resources[i].rid, resources[i].pid);
                     }
                 }
 
